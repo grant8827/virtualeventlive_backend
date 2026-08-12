0d6ef09273d12b59a7ae4536cf5ff89b74da70e2
Gregory Grant <gregorygrant@Gregorys-MBP.lan>
2026-08-12 13:30:41 -0400

    setup payout gateway

--- main.go
+++ main.go
@@ -48,7 +48,7 @@ func main() {
 	app.Use(cors.New(cors.Config{
 		AllowOrigins:     cfg.FrontendURL + ",http://localhost:5173",
 		AllowMethods:     "GET,POST,PUT,PATCH,DELETE,OPTIONS",
-		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
+		AllowHeaders:     "Origin, Content-Type, Accept, Authorization, X-Payout-Token",
 		AllowCredentials: true,
 	}))
 
